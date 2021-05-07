eba4b7fee3abc913acc73f9ba7c3f82fdaeae490
DanilErem <danilerem@gmail.com>
2021-05-07 23:01:25 +0300

    Bug fixed and add some coment

--- js/game.js
+++ js/game.js
@@ -1,12 +1,13 @@
 const canvas = document.getElementById("game");
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d"); 
 
-const ground = new Image();
+const ground = new Image(); 
 ground.src = "img/ground.png";
 
-const sw = 10;
-const sh = 20;
-const box = 48;
+const sw = 10; // Stack width
+const sh = 20; // Stack height
+const box = 48; // Cell size
+//Blocks array
 const blocks = [
 	[{x: 0, y: 0},	{x: -1, y: 0}, {x: 1, y: 0}, {x: 2, y: 0}],
 	[{x: 0, y: 0}, {x: 1, y: 1}, {x: 0, y: 1}, {x: 1, y: 0}],
@@ -19,24 +20,26 @@ const blocks = [
 
 let blockcount = blocks.length;
 
-let stack = new Array(sh).fill(0).map(() => new Array(sw).fill(0));
+//Stack array
+let stack = new Array(sh).fill(0).map(() => new Array(sw).fill(0)); 
 for (let x = 0; x < sw; x++) {
 	for (let y = 0; y < sh; y++) {
 		stack[y][x] = 0;	
 	}
 }
 
-let delay = 8;
-let count = 0;
-let blockX = 5;
-let blockY = 0;
-let curBlock = -1;
-let shift = "none";
-let dir = 0;
-let oldDir = 0;
+let delay = 8; // For speed of game
+let count = 0; // Block counnter
+let blockX = 5; // Block's cordinate X in stack arrray
+let blockY = 0; // Block's cordinate X in stack arrray
+let curBlock = -1; // Number of currrent block (-1 if none)
+let shift = "none"; // Left / right shift
+let dir = 0; // Block's rotation direction
+let oldDir = 0; // Save old direction
 
 document.addEventListener("keydown", direction);
 
+//Event listener
 function direction(event) {
 	if(event.keyCode == 37)
 		shift = "left";
@@ -50,6 +53,7 @@ function direction(event) {
 		delay = 1;
 }
 
+//Block rotation logic
 function Rotation(Block, direction) {
 	//let alpha = Math.PI * direction / 2.0;
 	let BlockRot = JSON.parse(JSON.stringify(Block));
@@ -74,6 +78,7 @@ function Rotation(Block, direction) {
 	return BlockRot;	 	
 }
 
+//Main function
 function drawGame() {
 
 	delay -= 1;
@@ -84,20 +89,24 @@ function drawGame() {
 	
 	ctx.drawImage(ground, 0, 0);
 
+	// Choose new block if it need
 	if (curBlock == -1) {
 		curBlock = blocks[Math.floor(Math.random() * blockcount)];
 		blockX = 5;
 		blockY = -1;
 		dir = 0;
+		//Block counter
 		count++; 
 	}
 	
 	curBlockRot = JSON.parse(JSON.stringify(curBlock));
 
+	//Rotaition if need
 	if (dir != 0) {
 		curBlockRot = Rotation(curBlock, dir);
 	}
 
+	//Left / right shift
 	let oldBlockX = blockX;
 	if (shift == "right") {
 		blockX += 1;		
@@ -105,54 +114,65 @@ function drawGame() {
 		blockX -= 1;
 	}
 
-	let oldBlockY = blockY;
-	blockY += 1;
+	if (oldBlockX != blockX || dir != oldDir) {
+		//Check collision for shift or rotate
+		for (let i = 0; i < curBlockRot.length; i++) {
+		 	let x = blockX + curBlockRot[i].x;
+		 	let y = blockY + curBlockRot[i].y;
 
-	for (let i = 0; i < curBlockRot.length; i++) {
-	 	let x = blockX + curBlockRot[i].x;
-	 	let y = blockY + curBlockRot[i].y;
-
-	 	if (x < 0 || x > sw-1) {
-	 		blockX = oldBlockX;
-	 		dir = oldDir;
-			curBlockRot = JSON.parse(JSON.stringify(curBlock));
-	 		break;
-	 	} 
+		 	if (x < 0 || x > sw-1 || (y >= 0 && y < sh && stack[y][x] == 1)) {
+		 		blockX = oldBlockX;
+		 		dir = oldDir;
+		 		//Array deep copy
+				curBlockRot = JSON.parse(JSON.stringify(curBlock));
+		 		break;
+		 	} 
+		}
 	}
 
 	if (oldDir != dir) {
 		oldDir = dir;
 	}
 
+	//Block always falls down
+	blockY += 1;
+
+	//Check collision
 	let flag = false;
 	for (let i = 0; i < curBlockRot.length; i++) {
 		let x = blockX + curBlockRot[i].x;
 		let y = blockY + curBlockRot[i].y;
 		if (y > sh-1) {
+			//Bottom of stack
 			flag = true;
 			break;
 		}
 		if (y >= 0 && stack[y][x] == 1) {
-			if (blockY == 0) {
-				clearInterval(game);
-			}
+			//Collision
 			flag = true;
 			break;
-		} 
-
+		}
 	}
 	
 	if (flag) {
-		blockY = oldBlockY;
-		for (let i = 0; i < curBlockRot.length; i++) {
-			let x = blockX + curBlockRot[i].x;
-			let y = blockY + curBlockRot[i].y;
-			stack[y][x] = 1;			
+		if (blockY == 0) {
+			//Game over
+			clearInterval(game);
 		}
-		curBlock = -1;
+		else {
+			//Add falled blocks in stack
+			for (let i = 0; i < curBlockRot.length; i++) {
+				let x = blockX + curBlockRot[i].x;
+				let y = blockY - 1 + curBlockRot[i].y;
+				if (y >= 0)
+					stack[y][x] = 1;			
+			}
+			//Need next block
+			curBlock = -1;
+		}
 	}
 
-		
+	//Draw blocks in stack	
 	for (let x = 0; x < sw; x++) {
 		for (let y = 0; y < sh; y++) {
 			if (stack[y][x] == 1) {
@@ -162,7 +182,7 @@ function drawGame() {
 		}
 	}
 
-		
+	//Drop full lines		
 	for (let y = 0; y < sh; y++) {
 		let i = y;
 		for (let x = 0; x < sw; x++) {
@@ -177,6 +197,7 @@ function drawGame() {
 		}
 	}
 
+	//Draw current block
 	if (curBlock != -1) {
 		for (let i = 0; i < curBlockRot.length; i++) {
 			let x = blockX + curBlockRot[i].x;
@@ -186,20 +207,14 @@ function drawGame() {
 		}		
 	}
 
-	// ctx.fillStyle = "black";
-	// ctx.fillRect(1 * box, 0, 1, box * 20);
-
-
-	// for (let i = 1; i++; i < 10) {
-	// 	ctx.fillStyle = "black";
-	// 	ctx.fillRect(i * box, 0, 1, box * 20);
-	// }
-
+	//Drawn counter
 	ctx.fillStyle = "white";
 	ctx.font = "50px Arial";
 	ctx.fillText(count, box * 1, box * 2);
 
+	//Drop "left" / "rigth" shift
 	shift = "none";
 }
 
+//FPS
 let game = setInterval(drawGame, 50);
